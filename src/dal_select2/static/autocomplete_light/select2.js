5a9ad347b788f8db528534119d2b1b978b6346ca
Rad Cirskis <nad2000@gmail.com>
2021-06-28 16:27:30 +1200

    reverted

--- src/dal_select2/static/autocomplete_light/select2.js
+++ src/dal_select2/static/autocomplete_light/select2.js
@@ -86,8 +86,7 @@ document.addEventListener('dal-init-function', function () {
             templateResult: result_template,
             templateSelection: selected_template,
             ajax: ajax,
-            // width: $element.attr('width') || '100%',  // null,
-            width: $element.attr('width') || null,
+            width: $element.attr('width') || '100%',  // null,
             tags: Boolean($element.attr('data-tags')),
         });
 
